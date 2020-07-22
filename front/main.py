e721342723d002f31206968816db02e43b582564
artur234 <jaskier234@gmail.com>
2020-07-22 14:00:07 +0200

    Simple exmaple of communicatio between users and front

--- front/main.py
+++ front/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    token = requests.get('http://users_server_1:5000/auth').text
+    return render_template('index.html', token=token)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
