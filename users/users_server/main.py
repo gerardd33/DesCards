617d1ed5df97065634b3332e31231ea8718f47b1
artur234 <jaskier234@gmail.com>
2020-08-02 14:37:20 +0200

    Moved flask server files to another folder

--- users/users_server/main.py
+++ users/users_server/main.py
@@ -3,12 +3,14 @@ import jwt
 
 app = Flask(__name__)
 
+
 # Authentciate user. returns access token
 @app.route('/auth')
 def hello_world():
     token = jwt.encode({'access': 'true'}, 'secret TODO better secret', algorithm='HS256')
     print(token)
-    return "{'access':'ok'}" # TODO Export algorithm to config
+    return "{'access':'ok'}"  # TODO Export algorithm to config
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
