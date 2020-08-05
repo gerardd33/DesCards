d72fda994515dfa0aabc2f27fa3a09d22fef52ee
gerardd33 <gerardd@interia.pl>
2020-08-05 22:36:59 +0200

    Created Flashcards project structure

---
+++ flashcards/src/test/java/com/descards/flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.descards.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
