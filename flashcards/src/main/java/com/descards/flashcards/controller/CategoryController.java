4d1f1ca17967ebb2335bdf4ff5d5fa65903e11b2
gerardd33 <gerardd@interia.pl>
2020-09-06 18:31:22 +0200

    Added class for handling HTTP responses

--- flashcards/src/main/java/com/descards/flashcards/controller/CategoryController.java
+++ flashcards/src/main/java/com/descards/flashcards/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package com.descards.flashcards.controller;
 
-import com.descards.flashcards.dto.CategoryDto;
 import com.descards.flashcards.facade.CategoryFacade;
+import com.descards.flashcards.util.HttpGuard;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class CategoryController {
@@ -16,12 +15,12 @@ public class CategoryController {
 	CategoryFacade categoryFacade;
 
 	@GetMapping("/categories")
-	List<CategoryDto> getAllCategories() {
-		return categoryFacade.getAllCategories();
+	ResponseEntity<?> getAllCategories() {
+		return HttpGuard.getResponse(() -> categoryFacade.getAllCategories());
 	}
 
 	@GetMapping("/category/{categoryId}")
-	List<String> getSpecialFields(@PathVariable long categoryId) {
-		return categoryFacade.getSpecialFields(categoryId);
+	ResponseEntity<?> getSpecialFields(@PathVariable long categoryId) {
+		return HttpGuard.getResponse(() -> categoryFacade.getSpecialFields(categoryId));
 	}
 }
