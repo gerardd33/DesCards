457348113f585d659b72a39486a7e84358e942af
artur234 <jaskier234@gmail.com>
2020-09-10 10:56:36 +0200

    Flashcard form is updating flashcards array

---
+++ front/front_app/src/components/FlashcardForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flashcard-form">
+    Front <input :value="flashcard.front" @input="update('front', $event.target.value)">
+    Back <input :value="flashcard.back" @input="update('back', $event.target.value)">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FlashcardForm',
+  props: ['flashcard'],
+  methods: {
+    update: function (key, value) {
+      var a = { ...this.flashcard, [key]: value }
+      console.log(a)
+      this.$emit('xd', a) 
+    }
+  },
+  computed: {
+    local() {
+      return this.flashcard ? this.flashcard : {}
+    },
+  },
+  created: function () {
+    console.log(this.flashcard)
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- front/front_app/src/views/Deck.vue
+++ front/front_app/src/views/Deck.vue
@@ -2,16 +2,25 @@
   <div class="flashcards-container">
     {{ deck_name }}<br>
     {{ page }}
-    <list :list="flashcards" :component="flashcard_componenet"></list>
+    <list :list="flashcards"
+      :component="flashcard_componenet"
+      @edit="edit"
+      >
+    </list>
     <button @click="prev">Poprzedni</button>
     <button @click="next">Następny</button>
+    <button>Dodaj</button>
+    <router-link to="/study">Nauka</router-link>
+    <flashcard-form :flashcard="flashcards[edited_key]" @xd="update_flashcard"></flashcard-form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import List from '@/components/List.vue'
-import Deck from '@/components/Deck.vue'
+// import Deck from '@/components/Deck.vue'
+import FlashcardForm from '@/components/FlashcardForm.vue'
+import Flashcard from '@/components/Flashcard.vue'
 //import  { getCookie } from '@/utils/cookies.js'
 import axios from 'axios'
 
@@ -21,14 +30,15 @@ export default {
     return {
       deck_name: '',
       flashcards: [ // TODO zmienić domyślną wartość
-        {id: 1, name: 'Talia 1'},
-        {id: 2, name: 'Talia 2'},
-        {id: 3, name: 'Talin 3'}
+        {id: 1, name: 'Talia 1', front:'asdf', back:'fdsa'},
+        {id: 2, name: 'Talia 2', front:'dwa', back:'fdsa'},
+        {id: 3, name: 'Talin 3', front:'trzy', back:'fdsa'}
       ],
-      flashcard_componenet: Deck,
+      flashcard_componenet: Flashcard,
       page: 0,
       limit: 20,
       last_page: false,
+      edited_key: 0
     }
   },
   computed: {
@@ -37,9 +47,17 @@ export default {
     }
   },
   components: {
-    List
+    List,
+    FlashcardForm
   },
   methods: {
+    update_flashcard: function (updated_flashcard) {
+      this.flashcards[this.edited_key] = updated_flashcard
+      console.log('updated', this.flashcards[this.edited_key])
+    },
+    edit: function (key) {
+      this.edited_key = key
+    },
     getFlashcards: function () {
       var vm = this
       var deckId = window.localStorage.getItem('deckId')
