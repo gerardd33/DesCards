fbcd545fa7a63eb42a56501aa2af2d3e0906add6
artur234 <jaskier234@gmail.com>
2020-09-10 16:13:24 +0200

    Added form for automatic flashcard generation

--- front/front_app/src/components/AutoCardForm.vue
+++ front/front_app/src/components/AutoCardForm.vue
@@ -7,33 +7,53 @@
         {{ category.name }}
       </option>
     </select>
-    <input type="checkbox"
+    <div class="special-fields"
       v-for="(field, index) in selected.specialFields"
       :key="index">
+      <input type="checkbox"
+        :id="index"
+        v-model="field.checked">
+      <label :for="index">{{ field.name }}</label>
+      <br>
+    </div>
+    <button @click="generate">generuj</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'AutoCardForm',
 //  props: ['entry', 'index'],
   data: function () {
     return {
-      categories: []
+      categories: [],
+      selected: {}
     }
   },
   methods: {
     remove: function () {
       this.deleted = true
       this.$emit('delete', this.index)
+    },
+    generate: function () {
+      console.log(this.selected)
     }
   },
   created: function () {
     // get categories
+    var vm = this
     axios.get('/api/categories')
     .then(function (response) {
       if (response.status === 200) {
-        vm.categories = response.data
+        vm.categories = response.data.map((value) => { 
+          return {...value, specialFields: value.specialFields.map((name) => {return {name, checked: false}})} 
+
+        })
+        if (vm.categories.length > 0) {
+          vm.selected = vm.categories[0]
+        }
       }
     })
   }
