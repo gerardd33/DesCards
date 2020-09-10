730c44c5d80521c6d9e3164d0be5b7e745f936d2
artur234 <jaskier234@gmail.com>
2020-09-10 11:12:18 +0200

    Added basic flashcard component and event handling in List component

---
+++ front/front_app/src/components/Flashcard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flashcard">
+    <span>{{ entry.id }}</span>
+    <span>{{ entry.front }}</span>
+    <span>{{ entry.back }}</span>
+    <span>{{ entry.interval }}</span>
+    <button @click="$emit('edit', index)">Edytuj</button>
+    <button @click="$emit('delete', index)">Usuń</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Flashcard',
+  props: ['entry', 'index'],
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- front/front_app/src/components/List.vue
+++ front/front_app/src/components/List.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="list">
     <component v-bind:is="component" 
-      v-for="entry in list"
-      v-bind:key="entry.id"
-      v-bind:entry="entry">
+      v-for="(entry, index) in list"
+      v-bind:key="index"
+      v-bind:index="index"
+      v-bind:entry="entry"
+      @edit="function (key) {$emit('edit', key)}"
+      @delete="function (key) {$emit('delete', key)}">
     </component>
   </div>
 </template>
