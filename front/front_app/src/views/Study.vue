5ae86877130039a92fbc19fb7472f646900bf493
artur234 <jaskier234@gmail.com>
2020-09-11 09:33:33 +0200

    Implemented machanism of fetching and navigating flashcards

--- front/front_app/src/views/Study.vue
+++ front/front_app/src/views/Study.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="study">
     {{ flashcardText }}
+    <button @click="prev">Prev</button>
+    <button @click="next">Next</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios'
+// import axios from 'axios'
 import { getFlashcards, commitChanges } from '@/utils/http.js'
 
 export default {
@@ -16,7 +18,9 @@ export default {
       flashcards: [{}],
       prevFlashcards: [],
       removedIds: [],
+      prevRemovedIds: [],
       editedIndexes: [],
+      prevEditedIndexes: [],
       currentFlashcardId: 0,
       isFront: true,
       bufferSize: 20
@@ -34,52 +38,58 @@ export default {
   components: {
   },
   methods: {
-    commitPrev: function () {
+    commit: function (cards, removedIds, editedIndexes, intervals=null) {
       var mapForUpdate = function (index) {
         return {
-          front: this.prevFlashcards[index].front,
-          back: this.prevFlashcards[index].back,
-          deckId: this.prevFlashcards[index].deckId
+          front: cards[index].front,
+          back: cards[index].back,
+          deckId: cards[index].deckId
         }
       }
-      var updated = this.editedIndexes.map(mapForUpdate)
+      var updated = editedIndexes.map(mapForUpdate)
       this.editedIndexes = []
       
       // TODO update intervals
 
-      commitChanges(updated, this.removedIds, interval)
-    },
-    getFlashcards: function () {
-      this.commitPrev()
-
-      this.prevFlashcards = this.flashcards
-
-      getFlashcards(this, 0, this.bufferSize)
-//    var vm = this
-//    var deckId = window.localStorage.getItem('deckId')
-//    axios.get('/api/deck', {params: {
-//      deckId,
-//      offset: 0,
-//      limit: 20,
-//      sortBy: 'interval',
-//      direction: 'asc'
-//    }})
-//    .then(function (response) {
-//      if (response.status === 200) {
-//        vm.flashcards = response.data
-//      }
-//    })
+      commitChanges(updated, removedIds, intervals)
     },
     getCurrentFlashcard: function () {
       var id = this.currentFlashcardId
+      console.log(id)
       if (id >= 0 && id < this.flashcards.length) {
         return this.flashcards[id]
       } else if (id < 0 && -id <= this.prevFlashcards.length) {
-        return this.prevFlashcards[this.prevFlashcards.length - id]
+        return this.prevFlashcards[this.prevFlashcards.length + id]
+      }
+    },
+    next: function () {
+      console.log(this.currentFlashcardId, this.prevFlashcards.length, this.flashcards.length)
+      if (this.currentFlashcardId < this.flashcards.length - 1) {
+        this.currentFlashcardId++
+      } else {
+        this.commit(this.prevFlashcards, this.prevRemovedIds, this.prevEditedIndexes)
+  
+        this.prevFlashcards = this.flashcards
+        this.prevRemovedIds = this.removedIds
+        this.removedIds = []
+        this.prevEditedIndexes = this.editedIndexes
+        this.editedIndexes = []
+  
+        getFlashcards(this, 0, this.bufferSize)
+        // TODO how errors work
+
+        this.currentFlashcardId = 0
+      }
+    },
+    prev: function () {
+      console.log(this.currentFlashcardId, this.prevFlashcards.length, this.flashcards.length)
+      if (-this.currentFlashcardId < this.prevFlashcards.length) {
+        this.currentFlashcardId--
       }
     }
   },
   created: function () {
+    getFlashcards(this, 0, this.bufferSize)
   }
 }
 </script>
